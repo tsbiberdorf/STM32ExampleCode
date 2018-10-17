6de0bd08dfa162c1e9abf14cb7c64f29c0095455
Terry Biberdorf <tbiberdorf@littelfuse.com>
2018-10-17 15:13:14 -0600

    got some of the CLI commands to work, the arrows do not work yet, not sure why?

--- Inc/consoleIO.h
+++ Inc/consoleIO.h
@@ -8,6 +8,16 @@
 #ifndef CONSOLEIO_H_
 #define CONSOLEIO_H_
 
+typedef enum _cliCmdType
+{
+	cliCmdType_np,
+	cliCmdType_wp_str,
+	cliCmdType_wp_int,
+	cliCmdType_i,
+	cliCmdType_ip_str,
+	cliCmdType_ip_int,
+};
+
 void consoleInit();
 void StartConsoleIO();
 
--- Src/consoleIO.c
+++ Src/consoleIO.c
@@ -15,10 +15,20 @@
 #include <sys/stat.h>
 #include <sys/unistd.h>
 
-static uint8_t tl_RxData[30];
-static uint8_t tl_TxData[30];
-static uint8_t tl_DataReady = 0;
-static uint8_t tl_TxDataReady = 0;
+#define CLI_BUFFER_SIZE (30)
+
+typedef struct ParseCli_s
+{
+	uint8_t cmd[CLI_BUFFER_SIZE];
+	uint8_t idx;
+}sParseCli_t;
+
+static sParseCli_t tl_ParseCli;
+static uint8_t tl_RxData[CLI_BUFFER_SIZE];
+static uint8_t tl_TxData[CLI_BUFFER_SIZE];
+static uint8_t tl_CliCmd[CLI_BUFFER_SIZE];
+
+volatile static uint8_t tl_TxDataReady = 0;
 extern UART_HandleTypeDef huart2;
 extern osMessageQId queueCLIHandle;
 
@@ -40,18 +50,23 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 
     xQueueSendFromISR( queueCLIHandle, &tl_RxData, &xHigherPriorityTaskWoken );
 
-//	tl_DataReady = 1;
 //	HAL_UART_Transmit_IT(&huart2,(uint8_t *)tl_RxData,1);
 }
 
+/**
+ * @details Start Console IO operations
+ */
 void StartConsoleIO()
 {
 	HAL_UART_Receive_IT(&huart2,(uint8_t*)tl_RxData,1);
 	HAL_UART_Transmit_IT( &huart2,(uint8_t *)tl_TxData, strlen((char *)tl_TxData));
 	tl_TxData[0] = '0';
-
+	memset(&tl_ParseCli,0,sizeof(sParseCli_t));
 }
 
+/**
+ * @details provide _write API to support printf calls
+ */
 int _write(int file, char *data, int len)
 {
 
@@ -81,26 +96,112 @@ int _write(int file, char *data, int len)
 }
 
 
+/**
+ * @details callback function called when the debug UART transmit port is idle
+ */
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
 {
 	tl_TxDataReady = 1;
 }
 
-static uint8_t tl_CliData[30];
+static uint8_t tl_CliRawData[30];
+static uint8_t tl_CliParseStr[30];
+
+/**
+ * @details perform direction arrow input commands from user
+ */
+static uint8_t DirectionArrowPressed(uint8_t *Direction)
+{
+	const uint8_t arrowUp[]={27,91,65};
+	const uint8_t arrowDown[]={27,91,66};
+	const uint8_t arrowRight[]={27,91,67};
+	const uint8_t arrowLeft[]={27,91,68};
+	const uint8_t deleteArrow[]={0x08,0x08,0x20,0x20,0x08,0x08}; // backspace 2 chars
+	const uint8_t deleteChar[]={0x08,0x20,0x08};
+
+	if( (Direction[0] == arrowUp[0]) && (Direction[1] == arrowUp[1]) )
+	{
+		if(Direction[2] == arrowUp[2])
+		{
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			Direction[2] = 0xff;
+		}
+		if(Direction[2] == arrowDown[2])
+		{
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			Direction[2] = 0xff;
+		}
+		if(Direction[2] == arrowRight[2])
+		{
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			Direction[2] = 0xff;
+		}
+		if(Direction[2] == arrowLeft[2])
+		{
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			Direction[2] = 0x08;
+		}
+	}
+	return Direction[2];
+}
+
+/**
+ * entry for Console Task.
+ */
 void StartConsoleTask(void const * argument)
 {
-	/* USER CODE BEGIN StartConsoleTask */
 	vTaskDelay(100);
+	const uint8_t deleteChar[]={0x08,0x20,0x08,0x0};
+	uint8_t direction[3]={0,0,0};
+	int32_t status;
 
 	printf("start Console Task\n");
+
 	/* Infinite loop */
 	for(;;)
 	{
-		xQueueReceive(queueCLIHandle,tl_CliData,portMAX_DELAY );
-		HAL_UART_Transmit_IT(&huart2,(uint8_t *)tl_CliData,1);
+		xQueueReceive(queueCLIHandle,tl_CliRawData,portMAX_DELAY );
+		direction[0] = direction[1];
+		direction[1] = direction[2];
+		direction[2] = tl_CliRawData[0];
+
+		tl_CliRawData[0] = DirectionArrowPressed(direction);
+
+		switch(*tl_CliRawData)
+		{
+		case 0xff:
+			break;
+		case 0x0A:
+			break;
+		case 0x0D:
+			vTaskDelay(25);
+			printf("\n%s\n",tl_ParseCli.cmd);
+			//			HAL_UART_Transmit_IT(&huart2,tl_ParseCli.cmd,tl_ParseCli.idx);
+			//			vTaskDelay(200);
+			//			HAL_UART_Transmit_IT(&huart2,crlf,2);
+			tl_ParseCli.idx = 0;
+			tl_ParseCli.cmd[0] = 0;
+			break;
+		case 0x08:
+			tl_ParseCli.cmd[--tl_ParseCli.idx] = 0x0;
+			printf("%s",deleteChar);
+//			HAL_UART_Transmit_IT(&huart2,deleteChar,3);
+			break;
+		default:
+			tl_ParseCli.cmd[tl_ParseCli.idx++] = *tl_CliRawData;
+			tl_ParseCli.cmd[tl_ParseCli.idx] = 0;
+//			printf("%s",tl_CliRawData[0]);
+			HAL_UART_Transmit_IT(&huart2,tl_CliRawData,1);
+//			vTaskDelay(25);
+		}
+//		HAL_UART_Transmit_IT(&huart2,(uint8_t *)tl_CliData,1);
 
 		osDelay(1);
 	}
-	/* USER CODE END StartConsoleTask */
+
 }
 
