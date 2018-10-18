5530c5fa700c39eaf36b7b4c902b192052125b5c
Terry Biberdorf <tbiberdorf@littelfuse.com>
2018-10-18 16:04:32 -0600

    sampling A0 ADC input

--- Src/adc1Task.c
+++ Src/adc1Task.c
@@ -8,15 +8,27 @@
 #include "stm32h7xx_hal.h"
 #include "cmsis_os.h"
 
+extern ADC_HandleTypeDef hadc1;
+
 void StartAdc1Task(void const * argument)
 {
+	uint32_t adcValue;
 	vTaskDelay(200);
 	printf("StartAdc3Task\n");
-  /* USER CODE BEGIN StartAdc3Task */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END StartAdc3Task */
+
+
+	/* Infinite loop */
+	for(;;)
+	{
+		HAL_ADC_Start(&hadc1);
+		if( HAL_ADC_PollForConversion(&hadc1,5) == HAL_OK )
+		{
+			adcValue = HAL_ADC_GetValue(&hadc1);
+		}
+		HAL_ADC_Stop(&hadc1);
+
+		printf("adc %d\n",adcValue);
+		vTaskDelay(500);
+	}
+	/* USER CODE END StartAdc3Task */
 }
